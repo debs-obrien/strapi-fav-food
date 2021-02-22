b42f257c2a0046e414300a0b8159e0bb9b084da6
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-02-22 16:05:03 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'da014cbb0e9e05edc78bb8dc7775e835'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '56fcdf34-4cab-455f-9f25-c2b6b92dbe4d'
+};
